181298f92c7114f2b21d0661f0768f7299ea0fb6
Adam Ferguson <afergus6@masonlive.gmu.edu>
2021-06-10 17:27:26 -0400

    added subplot titles

--- FlightGeneration/main/CreateFlights.py
+++ FlightGeneration/main/CreateFlights.py
@@ -51,12 +51,19 @@ if __name__ == '__main__':
 
     fig, axs = plt.subplots(7)
     axs[0].plot(df['cur_time'], df['cur_altitude'])
+    axs[0].set_title('cur_time vs cur_altitude')
     axs[1].plot(df['cur_time'], df['cur_airspeed'])
+    axs[1].set_title('cur_time vs cur_airspeed')
     axs[2].plot(df['cur_time'], df['vertical_speed'])
+    axs[2].set_title('cur_time vs vertical_speed')
     axs[3].plot(df['cur_time'], df['angle_of_attack'])
+    axs[3].set_title('cur_time vs angle_of_attack')
     axs[4].plot(df['cur_time'], df['flight_path_angle'])
+    axs[4].set_title('cur_time vs flight_path_angle')
     axs[5].plot(df['cur_time'], df['pitch_angle'])
+    axs[5].set_title('cur_time vs pitch_angle')
     axs[6].plot(df['cur_time'], df['roll'])
+    axs[6].set_title('cur_time vs roll')
     plt.show()
 
     # just looking at first 10 records
