10f384e6de89d3f3de5186d5608aeaa483431f77
akurian134 <37645689+akurian134@users.noreply.github.com>
2021-06-13 22:13:11 -0400

    Add line plot visualization function

--- FlightGeneration/main/ViewFlights.py
+++ FlightGeneration/main/ViewFlights.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import os
+
+# Create lineplots for various data fields (columns) over a single time column (time) and places the resulting output
+# into a "visuals" directory
+def lineplots_flight_data(cols, time, df):
+    cwd = os.getcwd()
+    fig, axs = plt.subplots(len(cols), figsize=(len(cols)*3, len(cols)*4))
+    for i in range(len(cols)):
+        axs[i].grid()
+        axs[i].plot(time, cols[i], data=df)
+        axs[i].set_ylabel(cols[i], rotation='horizontal', horizontalalignment='center', fontsize=10, labelpad=50)
+        axs[i].set_xlabel(time.name)
+        axs[i].set_xticks(np.arange(0, max(time), 1.0))
+        #axs[i].set_yticks(np.arange(min(df[cols[i]], max(df[cols[i]]), abs((max(df[cols[i]]) - min(df[cols[i]])/5)))))
+    plt.subplots_adjust(hspace=1, wspace=0.001)
+    fig.suptitle('Flight Data Simulations', y=0.92)
+    fig.savefig(cwd + '/visuals/' + 'flight_data.png')
+    plt.show()
 
 if __name__ == '__main__':
     df = pd.read_excel(open('stall_data.xlsx', 'rb'), sheet_name='sheet_data_1')
-
-    fig, axs = plt.subplots(7)
-    axs[0].plot(df['cur_time'], df['cur_altitude'])
-    axs[0].set_title('cur_time vs cur_altitude')
-    axs[1].plot(df['cur_time'], df['cur_airspeed'])
-    axs[1].set_title('cur_time vs cur_airspeed')
-    axs[2].plot(df['cur_time'], df['vertical_speed'])
-    axs[2].set_title('cur_time vs vertical_speed')
-    axs[3].plot(df['cur_time'], df['angle_of_attack'])
-    axs[3].set_title('cur_time vs angle_of_attack')
-    axs[4].plot(df['cur_time'], df['flight_path_angle'])
-    axs[4].set_title('cur_time vs flight_path_angle')
-    axs[5].plot(df['cur_time'], df['pitch_angle'])
-    axs[5].set_title('cur_time vs pitch_angle')
-    axs[6].plot(df['cur_time'], df['roll'])
-    axs[6].set_title('cur_time vs roll')
-    plt.show()
+    cols = ['cur_altitude', 'cur_airspeed', 'vertical_speed','angle_of_attack', 'flight_path_angle', 'pitch_angle', 'roll', 'rate_of_change_in_angle_of_attack']
+    time = df['cur_time']
+    lineplots_flight_data(cols, time, df) # Runs the function for ONLY the first sheet in the excel document
