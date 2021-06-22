d898979a6ca098a909a21876534b9de202ef0fd5
akurian134 <37645689+akurian134@users.noreply.github.com>
2021-06-21 23:27:57 -0400

    Created a new function to add lineplots. Updated main method to take column list and generate figures which nrow number of plots in each

--- FlightGeneration/main/ViewFlights.py
+++ FlightGeneration/main/ViewFlights.py
@@ -1,28 +1,31 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import os
+
+
+def lineplots_flight_data(cols, time, df, n):
+    cwd = os.getcwd()
+    fig, axs = plt.subplots(nrows, 1, figsize=(20, 15))
+    for col, ax in zip(cols, axs.flatten()):
+        ax.grid()
+        ax.plot(time, df[col])
+        ax.set_title(col + ' vs ' + time.name + '(s)')
+        ax.set_xticks(np.arange(min(time), max(time) + 10, 10.0))
+    plt.subplots_adjust(hspace=1, wspace=0.1)
+    fig.suptitle('Flight Data Simulations', y=0.92)
+    fig.savefig(cwd + '/visuals/' + 'flight_data' + str(n) + '.png')
+    plt.show()
+    plt.close()
+
 
 if __name__ == '__main__':
     df = pd.read_excel(open('stall_data.xlsx', 'rb'), sheet_name='sheet_data_0')
-
-    fig, axs = plt.subplots(10)
-    axs[0].plot(df['cur_time'], df['cur_altitude'])
-    axs[0].set_title('cur_time vs cur_altitude')
-    axs[1].plot(df['cur_time'], df['cur_airspeed'])
-    axs[1].set_title('cur_time vs cur_airspeed')
-    axs[2].plot(df['cur_time'], df['vertical_speed'])
-    axs[2].set_title('cur_time vs vertical_speed')
-    axs[3].plot(df['cur_time'], df['angle_of_attack'])
-    axs[3].set_title('cur_time vs angle_of_attack')
-    axs[4].plot(df['cur_time'], df['flight_path_angle'])
-    axs[4].set_title('cur_time vs flight_path_angle')
-    axs[5].plot(df['cur_time'], df['pitch_angle'])
-    axs[5].set_title('cur_time vs pitch_angle')
-    axs[6].plot(df['cur_time'], df['roll'])
-    axs[6].set_title('cur_time vs roll')
-    axs[7].plot(df['cur_time'], df['cur_avg_altitude'])
-    axs[7].set_title('cur_time vs cur_avg_altitude')
-    axs[8].plot(df['cur_time'], df['cur_avg_airspeed'])
-    axs[8].set_title('cur_time vs cur_avg_airspeed')
-    axs[9].plot(df['cur_time'], df['cur_avg_vertical_speed'])
-    axs[9].set_title('cur_time vs cur_avg_vertical_speed')
-    plt.show()
+    cols = ['cur_altitude', 'cur_airspeed', 'vertical_speed', 'angle_of_attack', 'flight_path_angle', 'pitch_angle',
+            'roll', 'cur_avg_altitude', 'cur_avg_airspeed', 'cur_avg_vertical_speed', 'cur_avg_flight_path_angle']
+    time = df['cur_time']
+    nrows = 4  # How many figures you want in a single plot
+    # Using list comprehension (https://www.geeksforgeeks.org/break-list-chunks-size-n-python/) to break up cols list into chunks based on nrows
+    split_cols = [cols[i * nrows:(i + 1) * nrows] for i in range((len(cols) + nrows - 1) // nrows)]
+    for n in range(len(split_cols)):
+        lineplots_flight_data(split_cols[n], time, df, n)
