eadf0224d5dcb979fd51a9bb9879bbfe239043cd
akurian134 <37645689+akurian134@users.noreply.github.com>
2021-06-30 19:36:08 -0400

    Created Random Forest script. Currently drops NaN values (which consistently appear with the flight_path_angle variable)

---
+++ FlightGeneration/main/RandomForest.py
@@ -0,0 +1,57 @@
+# Importing packages/functions
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score, mean_squared_error
+
+
+def prep_training_test_sets(filename, sheet_number, final_parameter_column, test_size, pred_variable):
+    dt = pd.read_excel(open(filename, 'rb'), sheet_name=sheet_number)
+    # Check for null values and drop them (most NaN values correspond to flight path angle)
+    dt.isna().any
+    dt = dt.dropna()
+    dt.isna().any
+    # Extracting static values (parameters)
+    params = dt.iloc[:, 0:final_parameter_column]
+    # Extracting flight data into its own dataframe
+    df = dt.iloc[:, final_parameter_column + 1:(len(dt) - 1)]
+    y = df[pred_variable]
+    x = df.drop(pred_variable, axis=1)
+    # Generating training and test sets
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
+    # Returning dimensions of full dataset, training, and test sets
+    print(f"Dataset dimensions: {df.shape}")
+    print(f"Training set dimensions: y:{y_train.shape} x:{x_train.shape}")
+    print(f"Test set dimensions: y:{y_test.shape} x:{x_test.shape}")
+    return x_train, x_test, y_train, y_test
+
+
+def run_random_forest(x_train, x_test, y_train, y_test, num_trees):
+    # Initialize Random Forest Regressor object
+    rfm = RandomForestRegressor(n_estimators=num_trees)
+    # Fit training data into regressor object
+    rfm_fit = rfm.fit(x_train, y_train)
+    # Run model on test data
+    pred = rfm.predict(x_test)
+    # Return Metrics
+    print(f"MSE: {mean_squared_error(y_test, pred)}")
+    print(f"RMSE: {np.sqrt(mean_squared_error(y_test, pred))}")
+    print(f"R2 Score: {r2_score(y_test, pred)}")
+
+
+if __name__ == '__main__':
+    # Parameters for functions
+    filename = 'stall_data.xlsx'
+    sheet_number = 'sheet_data_0'
+    final_parameter_column = 14  # Splits all the static parameters dataset. In this case, 14 is the last column with static data
+    test_size = 0.2  # Size of test dataset
+    pred_variable = 'vertical_speed'  # What we want the random forest to predict
+    num_trees = 100
+
+    # Prepare dataset for the model
+    [x_train, x_test, y_train, y_test] = prep_training_test_sets(filename, sheet_number, final_parameter_column,
+                                                                 test_size, pred_variable)
+    # Run the random forest model
+    run_random_forest(x_train, x_test, y_train, y_test, num_trees)
--- FlightGeneration/main/ViewFlights.py
+++ FlightGeneration/main/ViewFlights.py
@@ -6,11 +6,11 @@ import os
 
 def lineplots_flight_data(cols, time, df, n):
     cwd = os.getcwd()
-    fig, axs = plt.subplots(nrows, 1, figsize=(20, 15))
+    fig, axs = plt.subplots(nrows, 1, figsize=(16, 8))
     for col, ax in zip(cols, axs.flatten()):
         ax.grid()
         ax.plot(time, df[col])
-        ax.set_title(col + ' vs ' + time.name + '(s)')
+        ax.set_title(col + ' vs ' + time.name + ' (scale: 10 = 1s)')
         ax.set_xticks(np.arange(min(time), max(time) + 10, 10.0))
     plt.subplots_adjust(hspace=1, wspace=0.1)
     fig.suptitle('Flight Data Simulations', y=0.92)
@@ -20,9 +20,9 @@ def lineplots_flight_data(cols, time, df, n):
 
 
 if __name__ == '__main__':
-    df = pd.read_excel(open('stall_data.xlsx', 'rb'), sheet_name='sheet_data_0')
-    cols = ['cur_altitude', 'cur_airspeed', 'vertical_speed', 'angle_of_attack', 'flight_path_angle', 'pitch_angle',
-            'roll', 'cur_avg_altitude', 'cur_avg_airspeed', 'cur_avg_vertical_speed', 'cur_avg_flight_path_angle']
+    df = pd.read_excel(open('stall_data.xlsx', 'rb'), sheet_name='sheet_data_0', engine='openpyxl')
+    cols = ['cur_altitude', 'cur_airspeed', 'vertical_speed', 'roll', 'angle_of_attack', 'flight_path_angle', 'pitch_angle',
+            'cur_avg_altitude', 'cur_avg_airspeed', 'cur_avg_vertical_speed', 'cur_avg_flight_path_angle']
     time = df['cur_time']
     nrows = 4  # How many figures you want in a single plot
     # Using list comprehension (https://www.geeksforgeeks.org/break-list-chunks-size-n-python/) to break up cols list into chunks based on nrows
