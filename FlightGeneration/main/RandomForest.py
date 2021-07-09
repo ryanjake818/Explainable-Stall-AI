2df776feb3589439cfa18c262135e6fe3938f58f
akurian134 <37645689+akurian134@users.noreply.github.com>
2021-07-08 20:18:43 -0400

    Updated ViewFlights.py, CreateFlights.py, and RandomForest.py to take on the new csv file.

--- FlightGeneration/main/RandomForest.py
+++ FlightGeneration/main/RandomForest.py
@@ -8,12 +8,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_squared_error
 
 
-def prep_training_test_sets(filename, sheet_number, final_parameter_column, test_size, pred_variable):
-    dt = pd.read_excel(open(filename, 'rb'), sheet_name=sheet_number)
-    # Check for null values and drop them (most NaN values correspond to flight path angle)
-    #dt.isna().any
-    #dt = dt.dropna()
-    #dt.isna().any
+def prep_training_test_sets(filename, final_parameter_column, test_size, pred_variable):
+    dt = pd.read_csv(filename)
+   #dt = pd.read_excel(open(filename, 'rb'), sheet_name=sheet_number)
 
     # Extracting static values (parameters)
     params = dt.iloc[:, 0:final_parameter_column]
@@ -30,9 +27,11 @@ def prep_training_test_sets(filename, sheet_number, final_parameter_column, test
     set_names = ['x_train', 'x_test', 'y_train', 'y_test']
     sets = [x_train, x_test, y_train,  y_test]
     for n in range((len(sets))):
-        sets[n].to_excel(cwd + '/RandomForestOutputs/' + str(set_names[n]) + '.xlsx')
+        sets[n].to_csv(f"{cwd}/RandomForestOutputs/{str(set_names[n])}.csv'")
+        #sets[n].to_excel(cwd + '/RandomForestOutputs/' + str(set_names[n]) + '.xlsx')
 
     # Returning dimensions of full dataset, training, and test sets
+    print(f"Variable to Predict: {pred_variable}")
     print(f"Dataset dimensions: {df.shape}")
     print(f"Training set dimensions: y:{y_train.shape} x:{x_train.shape}")
     print(f"Test set dimensions: y:{y_test.shape} x:{x_test.shape}")
@@ -54,15 +53,15 @@ def run_random_forest(x_train, x_test, y_train, y_test, num_trees):
 
 if __name__ == '__main__':
     # Parameters for functions
-    filename = 'stall_data.xlsx'
-    sheet_number = 'sheet_data_0'
+    filename = 'stall_data.csv'
+    #sheet_number
     final_parameter_column = 14  # Splits all the static parameters dataset. In this case, 14 is the last column with static data
     test_size = 0.3  # Size of test dataset
     pred_variable = 'vertical_speed'  # What we want the random forest to predict
     num_trees = 100
 
     # Prepare dataset for the model
-    [x_train, x_test, y_train, y_test] = prep_training_test_sets(filename, sheet_number, final_parameter_column,
+    [x_train, x_test, y_train, y_test] = prep_training_test_sets(filename, final_parameter_column,
                                                                  test_size, pred_variable)
     # Run the random forest model
     run_random_forest(x_train, x_test, y_train, y_test, num_trees)
